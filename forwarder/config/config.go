42de16526cbe123c190f5ac5b2f73daaeb40cd82
chromerobv <robv@chromeriver.com>
2015-07-16 22:36:43 +0000

    map type

--- forwarder/config/config.go
+++ forwarder/config/config.go
@@ -46,7 +46,7 @@ func (config *LogstashForwarderConfig) AddContainerLogFile(container *docker.Con
 	file.Fields["docker.hostname"] = container.Config.Hostname
 	file.Fields["docker.name"] = container.Name
 	file.Fields["docker.image"] = container.Config.Image
-        file.Fields["docker.label"] = container.Config.Labels[cr_whoami]
+        file.Fields["docker.label"] = container.Config.Labels["cr_whoami"]
 
 	config.Files = append(config.Files, file)
 }
